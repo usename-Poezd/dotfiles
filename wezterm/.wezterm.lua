0f9ffccbf6a55cf81d04bb8a8dd6b79c932509a9
usename-Poezd <poezd.work@proton.me>
2026-02-03 23:13:23 +0300

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("JetBrainsMono NF")
+config.font = wezterm.font_with_fallback({
+	{
+		family = "JetBrainsMono NF", -- Укажи здесь свой шрифт
+		-- Эта строка отключает лигатуры:
+		harfbuzz_features = { "calt=0", "clig=0", "liga=0", "rlig=0" },
+	},
+})
 config.default_prog = { "tmux", "new", "-A", "-s", "main" }
 config.enable_tab_bar = false
 return config
